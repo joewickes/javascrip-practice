ab4a9326d277f4fbb715fc40d2c58fce1b485ddf
Joe Wickes <joe.c.wickes@gmail.com>
2021-03-20 22:17:18 -0400

    Remove BigInt for old version of Node

--- multiply-nums-as-strings.js
+++ multiply-nums-as-strings.js
@@ -7,5 +7,9 @@
 // Note: 100 randomly generated tests!
 
 function multiply(a, b) {
+
+  const parsedA = a % Math.floor(a) === 0 ? parseInt(a) : parseFloat(a);
+  const parsedB = b % Math.floor(b) === 0 ? parseInt(b) : parseFloat(b);
   
+  return parsedA * parsedB;
 }
