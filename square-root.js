882cdea2e7dd3064ea9fe0c415523c53b1300470
Joe Wickes <joe.c.wickes@gmail.com>
2021-03-21 22:58:24 -0400

    Get rid of unnecessary tries

--- square-root.js
+++ square-root.js
@@ -1,19 +1,3 @@
-function sroot(n, g) {
-  if (!g) {
-    g = n / 2.0;
-  }
-  let d = n / g;
-  let ng = (d + g) / 2.0;
-  if (g == ng) {          
-      return g;
-  }
-  return sroot(n, ng);
-}
-
-
-
-console.log(sroot(5));
-
 function sqroot(n) {
   let g = n / 2.0;
   let d = n / g;
